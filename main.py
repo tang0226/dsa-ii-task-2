ecbba5e1be971ea9582ae2bdf94eb02fd1c630e9
Toby Ang <58008827+tang0226@users.noreply.github.com>
2026-08-18 11:08:33 -0700

    Add distance table

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import csv
 from locations import Location, locations
 
 
+distance_table = [[0] * len(locations) for i in range(len(locations))]
+
+with open('distances.txt') as fp:
+  line = fp.readline().strip()
+  i = 0
+  while line:
+    strs = line.split(',')
+    for j in range(len(strs)):
+      distance_table[i][j] = distance_table[j][i] = float(strs[j])
+    i += 1
+    line = fp.readline().strip()
+
+
 class Package:
   def __init__(self, package_id, address, city, state, zip_code, deadline, weight, note):
     self.package_id = package_id
@@ -21,6 +34,7 @@ packages = []
 
 with open('packages.csv') as csvfile:
   reader = csv.reader(csvfile)
+  next(reader)
   for line in reader:
     packages.append(Package(*line))
 
