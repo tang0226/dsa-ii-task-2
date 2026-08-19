7c64c9987e1ff23a37267cec634c8e6e8c223a08
Toby Ang <58008827+tang0226@users.noreply.github.com>
2026-08-19 12:05:46 -0700

    Add classes and package routing

--- locations.py
+++ locations.py
@@ -1,36 +1,60 @@
+from hashtable import HashTable
+
 class Location:
-  def __init__(self, name, address, zip_code):
+  def __init__(self, location_id, name, address, zip_code):
+    self.location_id = location_id
     self.name = name
     self.address = address
     self.zip_code = zip_code
 
+  def __repr__(self):
+    return str(self.__dict__)
+  def __str__(self):
+    return f'{self.name}, {self.address}, {self.zip_code}'
 
-locations = [
-  Location('Western Governors University','4001 South 700 East','84107'),
-  Location('International Peace Gardens','1060 Dalton Ave S','84104'),
-  Location('Sugar House Park','1330 2100 S','84106'),
-  Location('Taylorsville-Bennion Heritage City Gov Off','1488 4800 S','84123'),
-  Location('Salt Lake City Division of Health Services ','177 W Price Ave','84115'),
-  Location('South Salt Lake Public Works','195 W Oakland Ave','84115'),
-  Location('Salt Lake City Streets and Sanitation','2010 W 500 S','84104'),
-  Location('Deker Lake','2300 Parkway Blvd','84119'),
-  Location('Salt Lake City Ottinger Hall','233 Canyon Rd','84103'),
-  Location('Columbus Library','2530 S 500 E','84106'),
-  Location('Taylorsville City Hall','2600 Taylorsville Blvd','84118'),
-  Location('South Salt Lake Police', '2835 Main St','84115'),
-  Location('Council Hall','300 State St','84103'),
-  Location('Redwood Park','3060 Lester St','84119'),
-  Location('Salt Lake County Mental Health','3148 S 1100 W','84119'),
-  Location('Salt Lake County/United Police Dept', '3365 S 900 W', '84119'),
-  Location('West Valley Prosecutor', '3575 W Valley Central Station bus Loop', '84119'),
-  Location('Housing Auth. of Salt Lake County', '3595 Main St', '84115'),
-  Location('Utah DMV Administrative Office', '380 W 2880 S', '84115'),
-  Location('Third District Juvenile Court', '410 S State St', '84111'),
-  Location('Cottonwood Regional Softball Complex', '4300 S 1300 E', '84117'),
-  Location('Holiday City Office', '4580 S 2300 E', '84117'),
-  Location('Murray City Museum', '5025 State St', '84107'),
-  Location('Valley Regional Softball Complex', '5100 South 2700 West', '84118'),
-  Location('City Center of Rock Springs', '5383 South 900 East #104', '84117'),
-  Location('Rice Terrace Pavilion Park', '600 E 900 South', '84105'),
-  Location('Wheeler Historic Farm', '6351 South 900 East', '84121')
+
+location_data = [
+  ['Western Governors University','4001 South 700 East','84107'],
+  ['International Peace Gardens','1060 Dalton Ave S','84104'],
+  ['Sugar House Park','1330 2100 S','84106'],
+  ['Taylorsville-Bennion Heritage City Gov Off','1488 4800 S','84123'],
+  ['Salt Lake City Division of Health Services ','177 W Price Ave','84115'],
+  ['South Salt Lake Public Works','195 W Oakland Ave','84115'],
+  ['Salt Lake City Streets and Sanitation','2010 W 500 S','84104'],
+  ['Deker Lake','2300 Parkway Blvd','84119'],
+  ['Salt Lake City Ottinger Hall','233 Canyon Rd','84103'],
+  ['Columbus Library','2530 S 500 E','84106'],
+  ['Taylorsville City Hall','2600 Taylorsville Blvd','84118'],
+  ['South Salt Lake Police', '2835 Main St','84115'],
+  ['Council Hall','300 State St','84103'],
+  ['Redwood Park','3060 Lester St','84119'],
+  ['Salt Lake County Mental Health','3148 S 1100 W','84119'],
+  ['Salt Lake County/United Police Dept', '3365 S 900 W', '84119'],
+  ['West Valley Prosecutor', '3575 W Valley Central Station bus Loop', '84119'],
+  ['Housing Auth. of Salt Lake County', '3595 Main St', '84115'],
+  ['Utah DMV Administrative Office', '380 W 2880 S', '84115'],
+  ['Third District Juvenile Court', '410 S State St', '84111'],
+  ['Cottonwood Regional Softball Complex', '4300 S 1300 E', '84117'],
+  ['Holiday City Office', '4580 S 2300 E', '84117'],
+  ['Murray City Museum', '5025 State St', '84107'],
+  ['Valley Regional Softball Complex', '5100 South 2700 West', '84118'],
+  ['City Center of Rock Springs', '5383 South 900 East #104', '84117'],
+  ['Rice Terrace Pavilion Park', '600 E 900 South', '84105'],
+  ['Wheeler Historic Farm', '6351 South 900 East', '84121'],
 ]
+
+locations = [Location(i, *location_data[i]) for i in range(len(location_data))]
+
+address_to_location = HashTable()
+for l in locations:
+  address_to_location.insert(l.address, l)
+
+def get_location_by_address(addr):
+  return address_to_location.get(addr)
+
+name_to_location = HashTable()
+for l in locations:
+  name_to_location.insert(l.name, l)
+
+def get_location_by_name(name):
+  return name_to_location.get(name)
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import csv
-from locations import Location, locations
+from datetime import datetime, date, time, timedelta
+
+from locations import Location, locations, get_location_by_address, get_location_by_name
+from hashtable import HashTable
+
+TODAY = date.today()
+
+def parse_time_str(time_str: str):
+  return datetime.strptime(time_str, '%I:%M %p').time()
+
+def add_today(t: time):
+  return datetime.combine(TODAY, t)
 
 
+# build distance table
 distance_table = [[0] * len(locations) for i in range(len(locations))]
 
 with open('distances.txt') as fp:
@@ -15,26 +27,262 @@ with open('distances.txt') as fp:
     line = fp.readline().strip()
 
 
-class Package:
-  def __init__(self, package_id, address, city, state, zip_code, deadline, weight, note):
+# Parent class for trucks and packages; both will have histories
+class TimedEntity:
+  def __init__(self, initial_time = time(0, 0, 0)):
+    self.current_time = add_today(initial_time)
+    self.history = []
+
+  def add_event(self, event_type: str, event_data = None):
+    self.history.append({'time': self.current_time.time(), 'type': event_type, 'data': event_data})
+
+  def wait(self, delta: timedelta):
+    self.current_time += delta
+
+  def wait_until(self, t: datetime):
+    if t < self.current_time:
+      raise ValueError('Entity cannot wait until previous time')
+    self.current_time = t
+
+
+class Package(TimedEntity):
+  def __init__(
+    self,
+    package_id,
+    address: str,
+    city: str,
+    state: str,
+    zip_code: str,
+    deadline: str,
+    weight,
+    note: str
+  ):
     self.package_id = package_id
     self.address = address
     self.city = city
     self.state = state
     self.zip_code = zip_code
-    self.deadline = deadline
-    self.weight = weight
+    self.deadline = 'EOD'
+    if deadline != 'EOD':
+      self.set_deadline(deadline)
+    self.arrival = add_today(time(0, 0, 0))
+    self.weight = int(weight)
     self.note = note
+    self.status = 'at hub'
+    if 'Delayed' in note:
+      self.status = 'delayed'
+    
+    super().__init__()
 
-class Truck:
+  def __repr__(self):
+    return str(self.__dict__)
+
+  def set_address(self, address: str):
+    self.address = address
+
+  def set_city(self, city: str):
+    self.city = city
+
+  def set_zip_code(self, zip_code: str):
+    self.zip_code = zip_code
+
+  def set_deadline(self, deadline: str):
+    self.deadline = add_today(parse_time_str(deadline))
+
+  def set_arrival(self, arrival: str):
+    if self.history:
+      raise RuntimeError('Cannot set arrival time of Package with history')
+    arrival_time = add_today(parse_time_str(arrival))
+    self.arrival = arrival_time
+
+    # seed the package's history with its arrival
+    self.wait_until(arrival_time)
+    self.add_event('arrived')
+    self.status = 'at hub'
+
+
+class Truck(TimedEntity):
   def __init__(self, truck_id):
-    pass
+    self.truck_id = truck_id
+    self.driver = None
+    self.packages = []
+    self.location = get_location_by_name('Western Governors University')
+    self.mileage = 0
 
-packages = []
+    # all trucks start at 8 AM
+    super().__init__(time(8, 0, 0))
 
+  def __str__(self):
+    return f'Truck {self.truck_id}'
+
+  def __repr__(self):
+    return str(self)
+  
+  def set_driver(self, driver: Driver):
+    self.add_event('set_driver', driver)
+    self.driver = driver
+
+  def remove_driver(self):
+    self.add_event('remove_driver', self.driver)
+    self.driver = None
+
+  def load(self, package: Package):
+    if package.arrival > self.current_time:
+      raise ValueError(f'Cannot load Package {package.package_id} into {self}: Package has not arrived yet')
+    if len(self.packages) >= 16:
+      raise ValueError(f'Cannot load Package {package.package_id} into {self}: Truck is full')
+    
+    self.packages.append(package)
+    self.add_event('load', package)
+
+    package.wait_until(self.current_time)
+    package.add_event('loaded', self)
+
+  def unload(self, package: Package):
+    if isinstance(package.deadline, datetime) and self.current_time > package.deadline:
+      print(f'ERROR: Package {package.package_id} was unloaded late')
+
+    self.packages.remove(package)
+    self.add_event('unload', package)
+
+    package.wait_until(self.current_time)
+    package.add_event('unloaded')
+    package.status = 'delivered'
+
+  def drive_to(self, new_loc: Location):
+    miles = distance_table[self.location.location_id][new_loc.location_id]
+    self.location = new_loc
+    self.mileage += miles
+    self.wait(timedelta(minutes=miles/(18/60)))
+    self.add_event('drive', {'location': new_loc, 'miles': miles, 'mileage': self.mileage})
+
+  def drive_to_address(self, address: str):
+    self.drive_to(get_location_by_address(address))
+
+  def drive_to_name(self, name: str):
+      self.drive_to(get_location_by_name(name))
+
+  def route(self):
+    for p in self.packages:
+      p.add_event('departed', self)
+      p.status = 'en route'
+
+    deadline_pkgs = list(filter(lambda p: p.deadline != 'EOD', self.packages))
+    deadline_pkg_ct = len(deadline_pkgs)
+
+    while self.packages:
+      search_list = self.packages
+      # go to packages with deadlines first
+      if deadline_pkg_ct:
+        search_list = deadline_pkgs
+
+      # use nearest neighbor algorithm
+      min_dist = 10**9
+      next_pkg = None
+
+      curr_loc_id = self.location.location_id
+
+      # find the closest location in the search list, then drive to that location
+      for p in search_list:
+        p_loc_id = get_location_by_address(p.address).location_id
+        dist = distance_table[curr_loc_id][p_loc_id]
+        if dist < min_dist:
+          min_dist = dist
+          next_pkg = p
+      self.drive_to_address(next_pkg.address)
+
+      # unload all appropriate packages at the current address
+      to_unload = filter(lambda p: p.address == next_pkg.address, self.packages)
+      for p in to_unload:
+        self.unload(p)
+        if p in deadline_pkgs:
+          deadline_pkgs.remove(p)
+          deadline_pkg_ct -= 1
+    
+    # return to WGU for loading
+    self.drive_to_name('Western Governors University')
+    
+
+
+class Driver:
+  def __init__(self, name):
+    self.name = name
+
+  def __repr__(self):
+    return str(self)
+
+  def __str__(self):
+    return self.name
+
+packages = HashTable()
 with open('packages.csv') as csvfile:
   reader = csv.reader(csvfile)
   next(reader)
   for line in reader:
-    packages.append(Package(*line))
+    package = Package(*line)
+    packages.insert(package.package_id, package)
 
+# special packages
+packages.get('6').set_arrival('9:05 AM')
+packages.get('9').set_address('410 S State St')
+packages.get('9').set_city('Salt Lake City')
+packages.get('9').set_zip_code('84111')
+packages.get('9').set_arrival('10:20 AM')
+packages.get('25').set_arrival('9:05 AM')
+packages.get('28').set_arrival('9:05 AM')
+packages.get('32').set_arrival('9:05 AM')
+
+# truck 1 will prioritize deadlined packages that aren't delayed
+batch1 = [packages.get(pid) for pid in [
+  # deadlined packages
+  '1', '13', '15', '29', '30', '31', '34', '37', '40',
+  # other packages
+  '2', '4', '5', '7', '8', '10', '11'
+]]
+
+# truck 2 will leave as soon as the delayed packages arrive
+batch2 = [packages.get(pid) for pid in [
+  # delayed, deadlined packages
+  '6', '25',
+  '14', '16', '20',  # <- must be delivered together
+  # other packages
+  '3', '18', '36', '38',  # <- must be on truck 2
+  '12', '17', '19', '21', '22', '23', '24'
+]]
+
+batch3 = [packages.get(pid) for pid in [
+  '9', # delayed package with wrong address
+  '26', '27', '28', '32', '33', '35', '39'
+]]
+
+truck1 = Truck('1')
+truck2 = Truck('2')
+truck3 = Truck('3')
+
+driver1 = Driver('Alice')
+driver2 = Driver('Bob')
+
+truck1.set_driver(driver1)
+truck2.set_driver(driver2)
+
+for p in batch1: truck1.load(p)
+
+truck2.wait_until(add_today(parse_time_str('9:05 AM')))
+for p in batch2: truck2.load(p)
+
+truck1.route()
+truck2.route()
+
+truck_to_vacate = None
+if truck1.current_time < truck2.current_time: truck_to_vacate = truck1
+else: truck_to_vacate = truck2
+
+truck3.wait_until(truck_to_vacate.current_time)
+
+repeat_driver = truck_to_vacate.driver
+truck_to_vacate.remove_driver()
+truck3.set_driver(repeat_driver)
+
+for p in batch3: truck3.load(p)
+truck3.route()
+  
